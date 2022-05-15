1ec6fd9dd18cc0a2d1c2905b3ccb673ee1ba9ef2
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-05-14 20:56:32 -0500

    Add basic bucket creation stack

--- CDK/Bucket/src/Bucket/BucketStack.cs
+++ CDK/Bucket/src/Bucket/BucketStack.cs
@@ -1,5 +1,8 @@
+using System;
 using Amazon.CDK;
+using Amazon.CDK.AWS.S3;
 using Constructs;
+using S3Bucket = Amazon.CDK.AWS.S3.Bucket;
 
 namespace Bucket
 {
@@ -7,7 +10,16 @@ namespace Bucket
     {
         internal BucketStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            String timeNowString = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            BucketProps bucketProps = new BucketProps
+            {
+                BucketName = $"bucket-{timeNowString}",
+                Versioned = true,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+                AutoDeleteObjects = true
+            };
+            
+            new S3Bucket(this, "MyBucket", bucketProps);
         }
     }
 }
