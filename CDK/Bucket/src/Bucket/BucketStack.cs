c1656503797bc9d185281e7828e3cdb693cb4209
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-05-14 20:22:47 -0500

    Add basic CDK project

---
+++ CDK/Bucket/src/Bucket/BucketStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Bucket
+{
+    public class BucketStack : Stack
+    {
+        internal BucketStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
